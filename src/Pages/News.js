daa8640935543f13280559ff36b7685a850e55db
jyotirmay <jyotirmay2000gupta@gmail.com>
2023-10-27 17:30:48 +0530

    adding new and video page

--- src/Pages/News.js
+++ src/Pages/News.js
@@ -1,30 +1,68 @@
-import React,{useState,useEffect}from 'react'
-import { UserEntertedInput } from '../Context/SearchContext'
+import React, { useState, useEffect } from "react";
+import { UserEntertedInput } from "../Context/SearchContext";
+import { Link } from "react-router-dom";
 
 export default function News() {
-    const {userinput} = UserEntertedInput()
-    const [dataarray, setdataarray] = useState([])
+  const { userinput } = UserEntertedInput();
+  const [dataarray, setdataarray] = useState([]);
+  const [moredata, setmoredata] = useState([]);
 
-    let apikey = 'a156523b095d46e4b5154363280ba4ba'
-    let api;
+  const apikey = process.env.React_App_news_api
+  let api;
+  let api2;
+  const cate1 = "top-headlines";
+  const cate2 = "everything";
 
-    useEffect(()=>{
-        api = `https://newsapi.org/v2/everything?apiKey=${apikey}&q=${userinput}&page=1`
-        fetchdata()
-        async function fetchdata(){
-            try {
-                let data = await fetch(api)
-                let response = await data.json()
-                console.log(response.articles)
-                setdataarray(response.articles)
-            } catch (error) {
-                console.log("unable to fetch data")
-            }
+  useEffect(() => {
+    api = `https://newsapi.org/v2/${cate1}?apiKey=${apikey}&q=${userinput}&page=1`;
+
+    fetchdata();
+    async function fetchdata() {
+      if (userinput) {
+        try {
+          let data = await fetch(api);
+          let response = await data.json();
+          setdataarray(response.articles);
+        } catch (error) {
+          console.log("unable to fetch data");
         }
-    },[userinput])
+      }
+    }
+    everything();
+  }, [userinput]);
+
+  async function everything() {
+    api2 = `https://newsapi.org/v2/${cate2}?apiKey=${apikey}&q=${userinput}&page=1`;
+    if (userinput) {
+      try {
+        let everydata = await fetch(api2);
+        let everydatares = await everydata.json();
+        setmoredata(dataarray.concat(everydatares.articles));
+      } catch (error) {
+        console.log("unable to fetch data");
+      }
+    }
+  }
   return (
-    <div className='news'>
-      <h3>there will be news here in this section of the page</h3>
-    </div>
-  )
+    <>
+      <div className="news">
+        {moredata.map((e) => {
+          let { description, title, url, urlToImage } = e;
+          return (
+            <Link className="link" target="_blank" to={url}>
+              <div className="universal news-box">
+                <div>
+                  <h2>{title}</h2>
+                  <p>{description}</p>
+                </div>
+                <div className="news-image-box">
+                  <img src={urlToImage} alt="" />
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </>
+  );
 }
--- src/Pages/Videos.js
+++ src/Pages/Videos.js
@@ -1,12 +1,73 @@
-import React from 'react'
-import { UserEntertedInput } from '../Context/SearchContext'
+import React, { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Loader from "../Components/Loader";
+import { UserEntertedInput } from "../Context/SearchContext";
+import { Link } from "react-router-dom";
 
 export default function Videos() {
-    const {userinput} = UserEntertedInput()
+  const { userinput } = UserEntertedInput();
+  const [videodata, setvideodata] = useState([]);
+  const [loading, setloading] = useState(false);
+  const [page, setpage] = useState(1);
+
+  const api_key = process.env.React_App_youtube_api;
+  let apiurl;
+
+  useEffect(() => {
+    if (userinput) {
+      setloading(true);
+      fetchdata();
+      async function fetchdata() {
+        apiurl = `https://www.googleapis.com/youtube/v3/search?key=${api_key}&q=${userinput}&part=snippet&type=video&page=${page}`;
+        let data = await fetch(apiurl);
+        let response = await data.json();
+        setloading(false);
+        setvideodata(response.items);
+      }
+    }
+  }, [userinput]);
+
+  async function fetchdata() {
+    apiurl = `https://www.googleapis.com/youtube/v3/search?key=${api_key}&q=${userinput}&part=snippet&type=video&page=${page + 1}`;
+    setloading(true);
+    try {
+      console.log("is it working or not ");
+      let data = await fetch(apiurl);
+      let response = await data.json();
+      setvideodata(videodata.concat(response.items));
+      setpage(page + 1);
+      setloading(false);
+    } catch (error) {
+      console.log("Dont have further data to fetch");
+    }
+  }
   return (
-    <div className='videos'>
-        <h4>{userinput}</h4>
-      <h4>here videos will be </h4>
+    <div className="videos">
+      <InfiniteScroll
+        dataLength={videodata.length}
+        next={fetchdata}
+        hasMore={true}
+        loader={<Loader />}
+        scrollThreshold={0.9}
+      >
+        {loading && <Loader />}
+        {videodata.map((e) => {
+          let { snippet,id } = e;
+          return (
+            <Link target="_blank" className="link" to={`https://www.youtube.com/watch?v=${id.videoId}`} >
+            <div className="universal videos-box">
+              <div>
+                <h3>{snippet.title}</h3>
+                <p>{snippet.description}</p>
+              </div>
+              <div className="video-image-box">
+                <img src={snippet.thumbnails.high.url} alt="" />
+              </div>
+            </div>
+            </Link>
+          );
+        })}
+      </InfiniteScroll>
     </div>
-  )
+  );
 }
