377393734c9a48c4cd99ce048d56d37ba50f1cda
jyotirmay <jyotirmay2000gupta@gmail.com>
2023-12-22 23:04:04 +0530

    old changes to provide search feature

--- src/App.js
+++ src/App.js
@@ -6,16 +6,23 @@ import Links from "./Pages/Links";
 import Imagedata from "./Pages/Images";
 import News from "./Pages/News";
 import Video from "./Pages/Videos";
+import First from "./Pages/First"
+import Suggestions from "./Components/Suggestions";
+import { UserEntertedInput } from "./Context/SearchContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
+  // const {userinpu} = UserEntertedInput
   return (
     <div className="App">
       <Searchinputfun> 
           <Navbar />
+          {/* <Suggestions/> */}
+          
         <Router>
           <Category />
           <Routes>
+            {/* <Route exact path="/" element={<First/>}/> */}
             <Route exact path="/" element={<Links />} />
             <Route exact path="/images" element={<Imagedata />} />
             <Route exact path="/news" element={<News/>}/>
---
+++ src/Pages/First.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Heading from '../Components/Heading'
+import Search from '../Components/Search'
+
+export default function First() {
+  return (
+    <div>
+      <Heading/>
+      <Search/>
+    </div>
+  )
+}
