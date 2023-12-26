9638a8aeb3bd4b3c91283b713f67423cd8638635
jyotirmay <jyotirmay2000gupta@gmail.com>
2023-12-26 20:52:21 +0530

    addfunc tailwind css

--- src/Components/Addfunc.js
+++ src/Components/Addfunc.js
@@ -2,15 +2,14 @@ import React from "react";
 import Weather from "../pictures/cloud_FILL0_wght400_GRAD0_opsz24.png";
 import todo from "../pictures/things_to_do_FILL0_wght400_GRAD0_opsz24.png";
 
-
 export default function Addfunc() {
   return (
-    <div className="universal addfunc">
-        <a target="_blank"  href="https://jyotirmaygithub.github.io/Weather-app/">
+    <div className="flex fixed right-8 top-5 space-x-3 ">
+      <a target="_blank" rel="noopener noreferrer" href="https://jyotirmaygithub.github.io/Weather-app/">
         <img src={Weather} alt="" />
-        </a>
-      <a target="_blank"  href="https://jyotirmaygithub.github.io/todo-/">
-      <img src={todo} alt="" />
+      </a>
+      <a target="_blank" rel="noopener noreferrer" href="https://jyotirmaygithub.github.io/todo-/">
+        <img src={todo} alt="" />
       </a>
     </div>
   );
