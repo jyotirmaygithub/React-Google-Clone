67a99bda0557b3efded3124fa768eb79e0efa23b
jyotirmay <jyotirmay2000gupta@gmail.com>
2023-12-26 20:59:05 +0530

    Move the heading component down by adding a top margin. (generated by blackbox.ai )

--- src/Components/Heading.js
+++ src/Components/Heading.js
@@ -3,7 +3,7 @@ import Logo from '../pictures/searcherlogo.png'
 
 export default function Heading() {
   return (
-    <div className='fixed left-7 flex space-x-1'>
+    <div className='fixed left-7 top-5 flex space-x-1'>
       <img src={Logo} alt="" />
       <h2 className='font-bold'>Searcher</h2>
     </div>
