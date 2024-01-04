6c381df5650750dda9b1fb4d6433054de2122e66
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-01-04 22:17:33 +0530

    feat : The search dropdown now depends on the display state from the context for visibility.

--- src/Context/SearchContext.js
+++ src/Context/SearchContext.js
@@ -5,8 +5,9 @@ const SearchInput = createContext();
 export function Searchinputfun(props) { // Use "props" instead of "{ Children }"
   const [userinput, setuserinput] = useState('');
   const [searchTerm ,setsearchTerm] = useState('')
+  const [display, setdisplay] = useState(false);
   return (
-    <SearchInput.Provider value={{ userinput, setuserinput, searchTerm,setsearchTerm }}>
+    <SearchInput.Provider value={{ userinput, setuserinput, searchTerm,setsearchTerm ,display,setdisplay}}>
       {props.children}
     </SearchInput.Provider>
   );
