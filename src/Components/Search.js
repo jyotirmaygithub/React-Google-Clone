bc57e3df4e320caf9c921ddd92b47462953cc6cf
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-01-04 22:18:06 +0530

    Feat : Updated the `handleInputChange` function to set the display state to true while typing

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,19 +1,24 @@
-import React,{useState} from 'react'
+import React from 'react'
 import {UserEntertedInput} from "../Context/SearchContext"
 
 
 export default function Search() {
-  const {setuserinput,searchTerm ,setsearchTerm} = UserEntertedInput()
+  const {setuserinput,searchTerm ,setsearchTerm,setdisplay} = UserEntertedInput()
 
   function writesubmit(e){
     e.preventDefault()
     setuserinput(searchTerm)
+    setdisplay(false)
+  }
+  function handleInputChange(e){
+    setsearchTerm(e.target.value);
+    setdisplay(true);
   }
   return (
     <>
     <div>
       <form onSubmit={writesubmit}>
-        <input placeholder='Search or type URL' onChange={(e)=>setsearchTerm(e.target.value)} className='fixed left-36 top-3 pl-3 p-[7px] w-[50vw] outline-none border-none  input-bar' type="search"  />
+        <input placeholder='Search or type URL' onChange={handleInputChange} className='fixed left-36 top-3 pl-3 p-[7px] w-[50vw] outline-none border-none rounded input-bar' type="search"  />
       </form>
     </div>
     </>
