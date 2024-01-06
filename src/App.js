d3ed62a9e8761cd5751c53196091373ffb0ed25d
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-01-06 23:24:49 +0530

    Route : home page route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Video from "./Pages/Videos";
 import Suggestions from "./Components/Suggestions";
 import { UserEntertedInput } from "./Context/SearchContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage"
 
 export default function App() {
   // const {userinpu} = UserEntertedInput
@@ -20,7 +21,8 @@ export default function App() {
         <Router>
           <Category />
           <Routes>
-            <Route exact path="/" element={<Links />} />
+            <Route exact path="/" element={<HomePage/>}/>
+            <Route exact path="/links" element={<Links />} />
             <Route exact path="/images" element={<Imagedata />} />
             <Route exact path="/news" element={<News/>}/>
             <Route exact path="/videos" element={<Video/>}/>
